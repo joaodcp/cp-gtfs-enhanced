793920005d7b0c9268521d4d2bb3242cdeafc788
joaodcp <67809513+joaodcp@users.noreply.github.com>
2026-04-26 17:29:59 +0100

    fix: use keys from environment

--- enhance.py
+++ enhance.py
@@ -28,9 +28,9 @@ def get_station_arrivals(station_id, date, start_time):
     res = requests.get(
         f"https://api-gateway.cp.pt/cp/services/travel-api/stations/{station_id}/timetable/{date}?start={start_time}",
         headers={
-            'x-api-key': 'ca3923e4-1d3c-424f-a3d0-9554cf3ef859',
-            'x-cp-connect-id': '1483ea620b920be6328dcf89e808937a',
-            'x-cp-connect-secret': '74bd06d5a2715c64c2f848c5cdb56e6b'
+            'x-api-key': os.getenv("CP_TRAVEL_API_KEY"),
+            'x-cp-connect-id': os.getenv("CP_TRAVEL_CONNECT_ID"),
+            'x-cp-connect-secret': os.getenv("CP_TRAVEL_CONNECT_SECRET")
         }
     )
     res.raise_for_status()
@@ -42,9 +42,9 @@ def get_trip_details(trip_ids):
         "https://api-gateway.cp.pt/cp/services/realtime-api/trains/details",
         json=trip_ids,
         headers={
-            'x-api-key': '5abe3d05-c76e-11cc-8ff1-cdc14135bb6f',
-            'x-cp-connect-id': 'edc64b3e659cfecf2f4e154dc6cef3c7',
-            'x-cp-connect-secret': '0bf2222674b8a419c8afe426d8a70465'
+            'x-api-key': os.getenv("CP_REALTIME_API_KEY"),
+            'x-cp-connect-id': os.getenv("CP_REALTIME_CONNECT_ID"),
+            'x-cp-connect-secret': os.getenv("CP_REALTIME_CONNECT_SECRET")
         }
     )
     res.raise_for_status()
@@ -55,9 +55,9 @@ def get_trip_shape(trip_id, max_retries=3):
     for attempt in range(max_retries):
         try:
             res = requests.get(CP_GIS_API_URL.format(trip_short_name=trip_id), headers={
-                'x-api-key': '8a208a6c-03e8-41f4-a39a-cec47cd7b446',
-                'x-cp-connect-id': 'edc64b3e659cfecf2f4e154dc6cef3c7',
-                'x-cp-connect-secret': '0bf2222674b8a419c8afe426d8a70465'
+                'x-api-key': os.getenv("CP_GIS_API_KEY"),
+                'x-cp-connect-id': os.getenv("CP_GIS_CONNECT_ID"),
+                'x-cp-connect-secret': os.getenv("CP_GIS_CONNECT_SECRET")
             }, timeout=30)
             if res.status_code != 200:
                 return None
